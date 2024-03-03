4af6347a994ee19878fe76ebeaee816895b48c7e
Aung Khant Myat <48421405+aungkhantmyat@users.noreply.github.com>
2024-03-03 23:53:51 +0630

    Update Monolingual_Summarization.py

--- pages/Monolingual_Summarization.py
+++ pages/Monolingual_Summarization.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import asyncio
-from streamlit_option_menu import option_menu
 
 # Define a function to load the tokenizer and model asynchronously
 async def load_model_and_tokenizer():
@@ -30,50 +29,36 @@ def summarize(text, max_length=150):
 # on session start
 st.set_page_config("NLP Summarization Demo", page_icon=":book:", layout="wide")
 
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Summarization",
-        options=["Monolingual Summarization", "Evaluation"],
-        icons=["lightbulb-fill","bar-chart-line-fill"],
-        menu_icon="file-earmark-text-fill",
-        default_index=0
+# layout ----------------
+# title and desc
+st.title("Myanmar News Summarization with Single-language Model")
+st.markdown(
+        ""
     )
+# input form area
+st.header("Input Area")
+input_form = st.empty()
 
-if selected == "Monolingual Summarization":
-    # layout ----------------
-    # title and desc
-    st.title("Myanmar News Summarization with Single-language Model")
-    st.markdown(
-        "This app allows you to test cutting edge news summarization models on \
-        text that is in-domain like news from BBC Burmese; and out-of-domain \
-        (OOD) such as the Korean Journal Of Medicine and ArXiv."
-    )
-    # input form area
-    st.header("Input Area")
-    input_form = st.empty()
+# output area
+col1, col2 = st.columns(2)
+with col1:
+    st.header("Full Input")
+    full_input = st.empty()
 
-    # output area
-    col1, col2 = st.columns(2)
-    with col1:
-        st.header("Full Input")
-        full_input = st.empty()
+with col2:
+    st.header("Summarization")
+    full_output = st.empty()
 
-    with col2:
-        st.header("Summarization")
-        full_output = st.empty()
-
-        # interactivity -----------------
-        # set the input form depending on the sidebar input mode
-        with input_form.form("Input"):
-            text_area = st.text_area("Your Text")
-            submitted = st.form_submit_button("Summarize!")
-    # run the model on submit
-    if submitted:
-        text_of_input = text_area
-        full_input.markdown(text_of_input)
-        # pass input to model and print output
-        with st.spinner("Summarizing..."):
-            text_of_output = summarize(text_of_input)
-            full_output.markdown(text_of_output)
-elif selected == "Evaluation":
-    st.title(f"You have selected {selected}")
+# interactivity -----------------
+# set the input form depending on the sidebar input mode
+with input_form.form("Input"):
+    text_area = st.text_area("Your Text")
+    submitted = st.form_submit_button("Summarize!")
+# run the model on submit
+if submitted:
+    text_of_input = text_area
+    full_input.markdown(text_of_input)
+    # pass input to model and print output
+    with st.spinner("Summarizing..."):
+        text_of_output = summarize(text_of_input)
+        full_output.markdown(text_of_output)
